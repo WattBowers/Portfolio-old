e2f5c29381bf6c3c7684cdae8d64c6a1847eb728
Jackson Watt-Bowers <Wattbowers@gmail.com>
2022-05-28 19:56:08 -0400

    building projects page

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -5,6 +5,7 @@ import checkmark from './Green-Check-Mark-Transparent-Background.png';
 import 'reactjs-popup/dist/index.css';
 import '../Card/Card.css';
 import { useSpring, animated } from 'react-spring';
+import Face from './Facerecognizer-portfolio.png'
 
 const Card = ({page, messageChange, emailChange, email, message, layoutChange}) => {
     const formId = 'iL7BUkVi'
@@ -45,21 +46,95 @@ const Card = ({page, messageChange, emailChange, email, message, layoutChange}) 
         (() => {
             if(page === 'home') {
                 return(
-                    <div className='w-50 dib br2 pa3 ma4 grow bw2 shadow-4'>
+                    <div className='w-50 dib br2 pa3 ma4 grow bw2 shadow-4 bg-moon-gray'>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris efficitur, tellus sed efficitur bibendum, quam quam laoreet magna, at accumsan ex neque vitae lectus. Nullam faucibus orci at ante aliquam, in pulvinar ligula rhoncus. Sed congue neque sit amet magna malesuada, eu dignissim enim mattis. Suspendisse vehicula mauris eu auctor imperdiet. Etiam commodo nibh eu tellus pellentesque, at ornare lorem vulputate. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quis mollis risus, ut dapibus dolor. Pellentesque vitae ultricies arcu. Nam vel felis dui. Fusce interdum vitae leo vulputate aliquam. Phasellus consequat leo ut tellus accumsan gravida. Vestibulum rhoncus iaculis nibh sit amet blandit. Curabitur non urna vel diam pellentesque viverra eu vitae ipsum. Maecenas lacus nisl, sagittis vitae dui sit amet, volutpat facilisis ante. Aliquam sit amet tortor pharetra, auctor massa at, pulvinar ipsum. Aenean euismod fringilla mi. Cras efficitur risus et dictum mattis. Donec non libero eget est tincidunt placerat. Proin consectetur, nunc tincidunt finibus varius, enim quam commodo velit, non blandit neque metus ac massa. Interdum et malesuada fames ac ante ipsum primis in faucibus. </p>
                     </div>
                 )
             }
             if(page === 'projects') {
                 return(
-                    <div className='w-60 dib br2 pa3 ma4 grow bw2 shadow-4'>
-                        <p>Test</p>
-                    </div>
+                    <div className='w-60 dib br2 pa3 ma4 grow bw2 shadow-4 bg-moon-gray h-50'>
+                      <div className='bg-white br4 w-100'>
+                        <div className='w-20 fl'>
+                          <Popup
+                          trigger={<button className="button1"> Face Finder </button>}
+                          modal
+                          nested
+                        >
+                          {close => (
+                            <div className="modal">
+                              <button className="close" onClick={close}>
+                                &times;
+                              </button>
+                              <div className="header"> Face Recognizer </div>
+                              <div className="content">
+                                {' '}
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, a nostrum.
+                                Dolorem, repellat quidem ut, minima sint vel eveniet quibusdam voluptates
+                                delectus doloremque, explicabo tempore dicta adipisci fugit amet dignissimos?
+                                <br />
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur sit
+                                commodi beatae optio voluptatum sed eius cumque, delectus saepe repudiandae
+                                explicabo nemo nam libero ad, doloribus, voluptas rem alias. Vitae?
+                              </div>
+                              <div className="actions">
+                                <a target="_blank" href='https://face-finding-project.herokuapp.com/'><button>Live site</button></a>
+                                <a target="_blank" href='https://github.com/WattBowers/facerecognizer-front'><button>Frontend Github Repo</button></a>
+                                <a target="_blank" href='https://github.com/WattBowers/facerecognizer-api'><button>Backend Github Repo</button></a>
+                              </div>
+                            </div>
+                          )}
+                          </Popup>
+                        </div>
+                        <div className='w-20 fl'>
+                          <h3>This is a description</h3>
+                        </div>
+                        <div className='w-33 fl'>
+                          <div className='recognizerbackground'>this is an image</div>
+                        </div>
+                      </div>
+                  
+                      
+                      
+                      
+                      
+                      <div>
+                      <Popup
+                        trigger={<button className="button"> The Boredom Butler </button>}
+                        modal
+                        nested
+                      >
+                        {close => (
+                          <div className="modal">
+                            <button className="close" onClick={close}>
+                              &times;
+                            </button>
+                            <div className="header"> The Boredom Butler </div>
+                            <div className="content">
+                              {' '}
+                              Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, a nostrum.
+                              Dolorem, repellat quidem ut, minima sint vel eveniet quibusdam voluptates
+                              delectus doloremque, explicabo tempore dicta adipisci fugit amet dignissimos?
+                              <br />
+                              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur sit
+                              commodi beatae optio voluptatum sed eius cumque, delectus saepe repudiandae
+                              explicabo nemo nam libero ad, doloribus, voluptas rem alias. Vitae?
+                            </div>
+                            <div className="actions">
+                              <a target="_blank" href='https://the-boredom-butler.herokuapp.com/'><button>Live site</button></a>
+                              <a target="_blank" href='https://github.com/WattBowers/Boredom-Butler'><button>Github Repo</button></a>
+                            </div>
+                          </div>
+                        )}
+                      </Popup>
+                      </div>
+                      </div>
+                    
                 )
             }
             if(page === 'skills') {
                 return(
-                    <div className='w-60 dib br2 pa3 ma4 grow bw2 shadow-4'>
+                    <div className='w-60 dib br2 pa3 ma4 grow bw2 shadow-4 bg-moon-gray'>
                         <p>Skills</p>
                         
                     </div>
@@ -68,18 +143,18 @@ const Card = ({page, messageChange, emailChange, email, message, layoutChange}) 
             }
             if(page === 'contact') {
                 return(
-                    <div className='w-50 dib br1 pa1 ma4 bw2 shadow-4 bg-{color: #C2B8B2}'>
+                    <div className='w-50 dib br1 pa1 ma4 bw2 shadow-4 bg-{color: #C2B8B2} bg-moon-gray'>
                     <main class="pa4 black-80">
                       <form class="measure center">
                         <fieldset id="sign_up" class="ba b--transparent ph0 mh0">
                           <legend class="f4 fw6 ph0 mh0">Contact Form</legend>
                           <div class="mt3">
                             <label class="db fw6 lh-copy f5 mb2">Return email</label>
-                            <input onChange={emailChange} class="pa2 input-reset ba bg-washed-green w-100" type="email" name="email-address"  id="email-address"/>
+                            <input onChange={emailChange} class="pa2 input-reset ba formcolor w-100" type="email" name="email-address"  id="email-address"/>
                           </div>
                           <div class="mv3">
                           <label  class="b db fw6 lh-copy f5">Message</label>
-                          <textarea onChange={messageChange} id="comment" name="comment" class=" db border-box hover-black w-100 measure ba b--black-20 pa2 br2 mb2 mt2 bg-washed-green" aria-describedby="comment-desc"></textarea>
+                          <textarea onChange={messageChange} id="comment" name="comment" class=" db border-box hover-black w-100 measure ba b--black-20 pa2 br2 mb2 mt2 formcolor" aria-describedby="comment-desc"></textarea>
                           </div>
                           
                         </fieldset>
--- src/components/Title.js
+++ src/components/Title.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Title = () => {
     return (
-        <div className='mt3 f2 lh-title mw3 mw7-ns center ph5-ns fw9'>
+        <div className='mt3 f2 lh-title mw3 mw7-ns center ph5-ns fw9 bg-moon-gray pt1 pb1 br3'>
             Hello, my name is Jackson and I am a Front-end Web developer. 
         </div>
     );
