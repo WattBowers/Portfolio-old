6085cb35e17bb4f89684b8e2d7eb5010b2c03ea4
Jackson Watt-Bowers <Wattbowers@gmail.com>
2022-05-30 17:18:17 -0400

    finishing pills

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Card/Card.css';
 
 const Title = () => {
     return (
-        <div className='mt3 f2 lh-title mw3 mw7-ns center ph5-ns fw9 bg-moon-gray pt1 pb1 br3'>
+        <div className='title mt3 f2 lh-title mw3 mw7-ns center ph5-ns fw9 bg-moon-gray pt1 pb1 br3'>
             Hello, my name is Jackson and I am a Front-end Web developer. 
         </div>
     );
